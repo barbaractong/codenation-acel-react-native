d9d487723da2fdcd1e622c5b3eaea73776d4ecd3
Barbara Chan Tong <barbarachantong@gmail.com>
2020-01-21 10:21:00 -0300

    add server folder and sha1 encrypter

--- src/main.js
+++ src/main.js
@@ -1,18 +1,27 @@
-const data = require('./answer.json')
+const data = require('./answer.json');
+const sha1 = require('js-sha1');
 
 encrypted = data.cifrado
 
 const CaesarDecryption = (textData, shift) => {
     var encryptedText = textData.toLowerCase()
-    var plainString = "";
+    var plainString = ""
     for(var i = 0; i < encryptedText.length; i++) {
         var encryptedChar = encryptedText.charCodeAt(i);
         if(encryptedChar >= 97 && encryptedChar <= 122) {
-            plainString += String.fromCharCode((encryptedChar - 97 - shift + 26) % 26 + 97 ); // no parenteses pega a posicao da letra quando soma 65 acha o numero em ascii
+            plainString += String.fromCharCode((encryptedChar - 97 - shift + 26) % 26 + 97 ); // pega a posicao da letra quando soma 65 acha o numero em ascii
         } else {
             plainString += String.fromCharCode(encryptedChar);
         }
     }
     return plainString;
 }
-console.log(CaesarDecryption(encrypted, 1))
+var decryptedData = (CaesarDecryption(encrypted, 1))
+
+const sha1Encrypter = (data) => {
+    return sha1(data)
+}
+
+const sha1Data = sha1Encrypter(decryptedData)
+
+console.log(sha1Data)
